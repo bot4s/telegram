169d4cebbc26e686acf35d2b5b8f5ae42a416539
Alfonso2 Peterssen <a2peterssen@gmail.com>
2015-12-20 16:14:02 +0100

    Removed obsolete UserOrGroupChat

--- src/main/scala/info/mukel/telegram/bots/api/UserOrGroupChat.scala
+++
@@ -1,7 +0,0 @@
-package info.mukel.telegram.bots.api
-
-/**
- * Created by mukel on 8/6/15.
- */
-// TODO: Use something like Either[User, GroupChat] instead (but causes problems with the JSON extractor/deserializer)
-case class UserOrGroupChat(id : Int)
