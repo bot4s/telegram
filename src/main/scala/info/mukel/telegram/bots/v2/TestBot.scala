0b7ea418c3d46d869ae7b1d08288c3b890990efa
Alfonso2 Peterssen <a2peterssen@gmail.com>
2016-05-17 01:13:08 +0200

    Add chat actions shortcuts

--- src/main/scala/info/mukel/telegram/bots/v2/TestBot.scala
+++ src/main/scala/info/mukel/telegram/bots/v2/TestBot.scala
@@ -1,12 +1,7 @@
 package info.mukel.telegram.bots.v2
 
-import java.net.URLEncoder
-
-import info.mukel.telegram.bots.v2.methods.{AnswerCallbackQuery, SendMessage}
-import info.mukel.telegram.bots.v2.model._
-import info.mukel.telegram.bots.v2.api.Implicits._
 import info.mukel.telegram.bots.v2.examples._
 
 object TestBot extends App {
-  WebhookBot.run()
+  ChatActionsBot.run()
 }
