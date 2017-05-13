c579f098420dc0390a85cf989ffb599ce4c6506e
Alfonso² Peterssen <a2peterssen@gmail.com>
2017-05-13 22:54:47 +0200

    Add AsyncUpdates trait to make all update handlers async.

---
+++ src/main/scala/info/mukel/telegrambot4s/api/AsyncUpdates.scala
@@ -0,0 +1,13 @@
+package info.mukel.telegrambot4s.api
+import info.mukel.telegrambot4s.models.Update
+
+import scala.concurrent.Future
+
+/**
+  * Asynchronify update handlers.
+  * It affects all handlers since all of them are called by onUpdate directly.
+  * Mix it last.
+  */
+trait AsyncUpdates extends BotBase with AkkaImplicits {
+  abstract override def onUpdate(u: Update): Unit = Future(super.onUpdate(u))
+}
