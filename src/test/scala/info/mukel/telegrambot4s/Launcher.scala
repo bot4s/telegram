247b7a23b2f8850c3460c2605b77d63a57423fee
Alfonso² Peterssen <a2peterssen@gmail.com>
2017-06-25 02:50:19 +0200

    Remove test launcher.

--- src/test/scala/info/mukel/telegrambot4s/Launcher.scala
+++
@@ -1,9 +0,0 @@
-package info.mukel.telegrambot4s
-
-import info.mukel.telegrambot4s.examples.RegexBot
-
-object Launcher {
-  def main(args: Array[String]): Unit = {
-    new RegexBot("211113992:AAGlXLvA9lwhltbdMBUYBasBmw1giegeqfc").run()
-  }
-}
