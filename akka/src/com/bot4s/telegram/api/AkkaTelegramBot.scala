15fca3ee65f9923d410b91972dcc051abf50ccf2
Alfonso Peterssen <a2peterssen@gmail.com>
2018-10-21 22:33:37 +0200

    Remove AkkaTelegramBot harcdoded client.

--- akka/src/com/bot4s/telegram/api/AkkaTelegramBot.scala
+++ akka/src/com/bot4s/telegram/api/AkkaTelegramBot.scala
@@ -4,10 +4,7 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import com.bot4s.telegram.clients.AkkaHttpClient
 
-trait AkkaTelegramBot extends BotBase with GlobalExecutionContext with AkkaDefaults {
-  def token: String
-  override val client: RequestHandler = new AkkaHttpClient(token)
-}
+trait AkkaTelegramBot extends BotBase with GlobalExecutionContext with AkkaDefaults
 
 trait AkkaImplicits {
   implicit val system: ActorSystem
