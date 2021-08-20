940ccc3894a49b93a41cc27715bb5fa41e7574d2
ex0ns <g.tournigand@gmail.com>
2021-08-20 13:25:41 +0200

    Add ChatMemberRestricted support
    Description: https://core.telegram.org/bots/api\#chatmemberrestricted
    
    Status will always be restricted

--- core/src/com/bot4s/telegram/models/MemberStatus.scala
+++ core/src/com/bot4s/telegram/models/MemberStatus.scala
@@ -2,5 +2,5 @@ package com.bot4s.telegram.models
 
 object MemberStatus extends Enumeration {
   type MemberStatus = Value
-  val Creator, Administrator, Member, Left, Kicked = Value
+  val Creator, Administrator, Member, Restricted, Left, Kicked = Value
 }
