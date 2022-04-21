7752d06ffd0ec39c5632fd8a97ed6ec7cdff0ebb
ex0ns <ex0ns@users.noreply.github.com>
2022-04-21 22:10:55 +0200

    Update ReplyMarkup.scala

--- core/src/com/bot4s/telegram/models/ReplyMarkup.scala
+++ core/src/com/bot4s/telegram/models/ReplyMarkup.scala
@@ -81,7 +81,7 @@ object KeyboardButton {
  * @param selective             Boolean Optional Use this parameter if you want to show the keyboard to specific users only.
  *                                      Targets:
  *                                      1) users that are @mentioned in the text of the Message object;
- *                                      2) if the bot's message is a reply (has replyToMessage_id), sender of the original message.
+ *                                      2) if the bot's message is a reply (has replyToMessageId), sender of the original message.
  */
 case class ReplyKeyboardMarkup(
   keyboard: Seq[Seq[KeyboardButton]],
