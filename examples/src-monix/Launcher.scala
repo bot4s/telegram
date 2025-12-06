4117e91900fd5468ee1bdd5668959efacf3eb60f
Mike Limansky <mike.limansky@gmail.com>
2025-12-06 16:40:02 +0300

    Migrate to sttp 4

--- examples/src-monix/Launcher.scala
+++ examples/src-monix/Launcher.scala
@@ -1,12 +1,13 @@
 import cats.effect._
 import monix.eval._
+import sttp.client4.httpclient.monix.HttpClientMonixBackend
 
 object Launcher extends TaskApp {
 
   def run(args: List[String]): Task[ExitCode] =
     args.toList match {
       case List("EchoBot", token) =>
-        new EchoBot(token).run().as(ExitCode.Success)
+        HttpClientMonixBackend().flatMap(b => new EchoBot(token, b).run()).as(ExitCode.Success)
       case List(name, _) =>
         Task.raiseError(new Exception(s"Unknown bot $name"))
       case _ =>
